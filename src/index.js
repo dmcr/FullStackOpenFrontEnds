769827b5965c7cc47de42bfa7d46945b1f0eabfc
Duncan McRobert <dmcrobert@live.co.uk>
2020-02-25 18:00:12 -0500

    refactored code to support passing of parts as object

--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const parts = [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   
     return (
       <div>
         <Header title={course}/>
-        <Content content={[[part1, exercises1], [part2, exercises2], [part3, exercises3]]}/>
-        <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+        <Content contents={parts}/>
+        <Total total={parts}/>
       </div>
     )
   }
@@ -25,25 +33,26 @@ const App = () => {
     )
   }
 
-  const Content = ({content}) => {
+  const Content = ({contents}) => {
+    console.log(contents);
     return (
     <div>
-      <Part title={content[0][0]} numberOf={content[0][1]} />
-      <Part title={content[1][0]} numberOf={content[1][1]} />
-      <Part title={content[2][0]} numberOf={content[2][1]} />
+      <Part content={contents[0]} />
+      <Part content={contents[1]} />
+      <Part content={contents[2]} />
     </div>
     )
   }
 
-  const Part = ({title}, {numberOf}) => {
+  const Part = ({content}) => {
     return (
-      <p>{title} {numberOf}</p>
+      <p>{content.name} {content.exercises}</p>
     )
   }
 
-  const Total = ({exercises1, exercises2, exercises3}) => {
+  const Total = ({total}) => {
     return (
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    <p>Number of exercises {total[0].exercises + total[1].exercises + total[2].exercises}</p>
     )
   }
 
