789d4ebe4cc08e4702a673dd9972e7e25ac33125
Duncan McRobert <dmcrobert@live.co.uk>
2020-02-25 13:25:34 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+    return (
+        <div>
+            <p>Hello {props.name}, you are {props.age} years old</p>
+        </div>
+    )
+}
+
+const App = () => {
+    const name = 'Test';
+    const age = 1;
+
+    return (
+        <div>
+            <h1>Greetings</h1>
+            <Hello name="Duncan" age="27" />
+            <Hello name={name} age={age} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
