7fd19191dfa5cdf6d5f5ac41b2fd2715adf563bc
Duncan McRobert <dmcrobert@live.co.uk>
2020-06-08 14:29:50 -0400

    Login form hidden on login. Login form is now replace by logged in user.name

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -17,11 +19,23 @@ const App = () => {
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({ username, password })
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+    catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }
 
-  return (
-    <div>
-      <form onSubmit={handleLogin}>
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
         <div>
           username
             <input
@@ -42,6 +56,17 @@ const App = () => {
             <button type="submit">login</button>
         </div>
       </form>
+  )
+
+  return (
+    <div>
+      <div>{errorMessage}</div>
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in</p>
+        </div>
+      }
       <div>
         <h2>blogs</h2>
         {blogs.map(blog =>
